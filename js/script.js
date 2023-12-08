2227cb293d9394d8a1625128926d4521a9e13cf9
LilTibie <153116008+LilTibie@users.noreply.github.com>
2023-12-08 12:16:03 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*************************************************************************************************************************************************************************************************************************************************************
+    About Tab
+**************************************************************************************************************************************************************************************************************************************************************/
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+    tablinks.classList.remove("active-link"); 
+}
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
